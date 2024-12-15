afdfaf1b24b853704fb2ecf4cce0fefaf316c685
Desire <work@desiremcarmona.com>
2024-12-15 17:32:40 +0100

    ✅ Created a modal to confirm users action to delete; created macros to reorganize the UI 🔜 More refactors & UI layout improvement

---
+++ static/main.js
@@ -0,0 +1,7 @@
+let taskToDelete = null;
+
+function setTaskId(task_id) {
+  taskToDelete = task_id; // Save task ID
+  document.getElementById("dialog").open = true; // Open modal
+  document.getElementById("delBtn").href = "/del/" + taskToDelete; // Set the delete button of the modal
+}
